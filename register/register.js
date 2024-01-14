8d1f4f0d490c88ab8394277dfb581590f2c09ff9
CosminBS <butucosmin95@gmail.com>
2024-01-14 21:01:23 +0200

    Update register form

--- register/register.js
+++ register/register.js
@@ -29,30 +29,42 @@ function registerUser() {
     if(!isValideEmail(email)){
         errorMessage.push('Invalid email syntax')
         email.style.border = '2px solid red'
+        email.placeholder = 'Invalid email syntax'
+        email.style.fontSize = '16px'
     }
 
     if(!isValideUsername(username)){
         errorMessage.push('Username must contain at least 6 characters long')
         username.style.border = '2px solid red'
+        username.placeholder = 'Username must contain at least 6 characters long'
+        username.style.fontSize = '16px'
     }
 
     if(!isValidePassword(password)){
         errorMessage.push('Password must contain at least 6 characters long')
         password.style.border = '2px solid red'
+        password.placeholder = 'Password must contain at least 6 characters long'
+        password.style.fontSize = '16px'
     }
 
     if(!isValideFirstName(firstName)){
         errorMessage.push('First name must contain at least 2 character long')
         firstName.style.border = '2px solid red'
+        firstName.placeholder = 'First name must contain at least 2 character long'
+        firstName.style.fontSize = '16px'
     }
 
     if(!isValideLastName(lastName)){
-        errorMessage.push('Last name must contain at least 2 character long')
+        errorMessage.push('Last name must contain at least 2 characters long')
+        lastName.placeholder = 'Last name must contain at least 2 characters long'
+        lastName.style.fontSize = '16px'
         lastName.style.border = '2px solid red'
     }
 
     if(!isValideAge(age)){
         errorMessage.push('Age must be between 18 and 65')
+        age.placeholder = 'Age must be between 18 and 65'
+        age.style.fontSize = '16px'
         age.style.border = '2px solid red'
     }
 
@@ -70,6 +82,7 @@ function registerUser() {
 function isValideEmail(email){
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     email.style.border = '2px solid green';
+    email.style.fontSize = '14px'
 
     return emailRegex.test(email.value)
     
@@ -78,6 +91,7 @@ function isValideEmail(email){
 function isValideUsername(username){
     if(username.value.length >= 6){
         username.style.border = '2px solid green';
+        username.style.fontSize = '14px';
         return true    
     }
 };
@@ -85,6 +99,7 @@ function isValideUsername(username){
 function isValidePassword(password){
     if(password.value.length >= 6){
         password.style.border = '2px solid green';
+        password.style.fontSize = '14px';
         return true
     }
 };
@@ -92,6 +107,7 @@ function isValidePassword(password){
 function isValideFirstName(firstName){
     if(firstName.value.length >= 2){
         firstName.style.border = '2px solid green';
+        firstName.style.fontSize = '14px';
         return true
     }
 };
@@ -99,13 +115,15 @@ function isValideFirstName(firstName){
 function isValideLastName(lastName){
     if(lastName.value.length >= 2){
         lastName.style.border = '2px solid green';
+        lastName.style.fontSize = '14px';
         return true
     }
 };
 
 function isValideAge(age){
-    if(age.value > 18 && age.value < 65){
+    if(age.value >= 18 && age.value <= 65){
         age.style.border = '2px solid green';
+        age.style.fontSize = '14px';
         return true
     }
 }
